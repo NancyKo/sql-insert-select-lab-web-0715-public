e81a0fe0aeff16b445808b084feef7626d7c9803
NancyKo <konancys@gmail.com>
2015-08-13 20:00:36 -0400

    Adding cat schema and select statements to spec

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO cats (
+  id, name, age, breed, gender, temperment, declawed
+) 
+VALUES 
+(1, 'Maru', 6, 'Scottish Fold', 'M', 'Playful', 0 ),
+(2, 'Nala', 3, 'Siamese', 'F', 'Diva', 0),
+(3, 'Grumpy Cat', 2, 'Ragdoll', 'F', 'Friendly', 1),
+(4, 'Lil Bub', 3, 'Dwarf', 'F', 'Friendly', 1);
